23c1cd119d22f24eb263a7ca072326685003afd5
buihainghiaaa <95730026+buihainghiaaa@users.noreply.github.com>
2022-02-06 15:20:01 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -294,7 +294,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "https://www.youtube.com/watch?v=64uYDyrnXsM?controls=0&loop=1&autoplay=1");
+  ifrm.setAttribute("src", "https://www.youtube.com/embed/F7UQN4wIsNE?controls=0&loop=1&autoplay=1");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   ifrm.style.border = 'none';
